6a2dd68219a9368a45d4a77355f3d4bd01ba164f
Max Ershov <maxx95.95@gmail.com>
2020-12-31 23:25:00 +0300

    fix: err on update

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var a = document.getElementsByTagName('a');
+
 for (var i = 0; i < a.length; ++i) {
-    // a[i].dataset.title = a[i].title;
-    a[i].title = a[i].href;
-    a[i].href = "javascript:void(0);";
+
+    // fix err on update
+    if (a[i].href != "javascript:void(0);") {
+        a[i].title = a[i].href;
+        a[i].href = "javascript:void(0);";
+    }
 }
