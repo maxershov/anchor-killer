81f84ba824fdf3d9f1f1a73422051615c5bbab96
Max Ershov <maxx95.95@gmail.com>
2021-01-02 01:36:10 +0300

    fix bg err and chg icons

--- background.js
+++ background.js
@@ -1,29 +1,42 @@
 var state = false;
 
+
 // check saved state and apply on page update
 chrome.tabs.onUpdated.addListener(function (tabId) {
 
     if (state) {
-        chrome.tabs.executeScript(tabId, { file: "script.js" });
+        disableHref();
     }
 });
 
 
 function enableHref() {
 
-    chrome.browserAction.setIcon({ path: "img/icon16.png" })
     chrome.tabs.executeScript(null, { file: "fix.js" });
+    chrome.browserAction.setIcon({path: {
+        "16": "img/icon16.png",
+        "32": "img/icon32.png",
+        "48": "img/icon48.png",
+        "128": "img/icon128.png"
+    }});
 }
+
 
 function disableHref() {
 
     chrome.tabs.executeScript(null, { file: "script.js" });
-    chrome.browserAction.setIcon({ path: "img/iconActive16.png" })
+    
+    chrome.browserAction.setIcon({path: {
+        "16": "img/iconActive16.png",
+        "32": "img/iconActive32.png",
+        "48": "img/iconActive48.png",
+        "128": "img/iconActive128.png"
+    }});
 }
 
 
 // check or save state from popup 
-chrome.runtime.onMessage.addListener(function (request, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
     if (request.payload !== "check") {
         state = request.payload;
