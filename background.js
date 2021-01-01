9a2e98e6e72d4aa63bed8bc5b896a4a259acd129
Max Ershov <maxx95.95@gmail.com>
2021-01-02 00:41:00 +0300

    feat: add dynamic icon

--- background.js
+++ background.js
@@ -1,15 +1,39 @@
 var state = false;
 
 // check saved state and apply on page update
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (state) chrome.tabs.executeScript(tabId, { file: "script.js" });
+chrome.tabs.onUpdated.addListener(function (tabId) {
+
+    if (state) {
+        chrome.tabs.executeScript(tabId, { file: "script.js" });
+    }
 });
 
 
+function enableHref() {
+
+    chrome.browserAction.setIcon({ path: "img/icon16.png" })
+    chrome.tabs.executeScript(null, { file: "fix.js" });
+}
+
+function disableHref() {
+
+    chrome.tabs.executeScript(null, { file: "script.js" });
+    chrome.browserAction.setIcon({ path: "img/iconActive16.png" })
+}
+
+
 // check or save state from popup 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sendResponse) {
+
     if (request.payload !== "check") {
         state = request.payload;
+
+        if (state) {
+            disableHref();
+        } else {
+            enableHref();
+        }
+
     } else {
         sendResponse({ payload: state });
         return true;
--- popup.js
+++ popup.js
@@ -1,8 +1,10 @@
 var checkboxState = false;
 
+
 chrome.runtime.sendMessage({
     payload: "check"
 }, function (response) {
+
     checkboxState = response.payload;
 
     var checkbox = document.getElementById('checkbox');
@@ -10,31 +12,20 @@ chrome.runtime.sendMessage({
 });
 
 
-function killLinks() {
-    chrome.tabs.executeScript(null, { file: "script.js" });
-
-    chrome.runtime.sendMessage({ payload: true });
-
-    window.close();
-}
-
-function fixLinks() {
-    chrome.tabs.executeScript(null, { file: "fix.js" });
-
-    chrome.runtime.sendMessage({ payload: false });
-
-    window.close();
-}
-
 
 document.addEventListener('DOMContentLoaded', function () {
     var checkbox = document.getElementById('checkbox');
+
     checkbox.addEventListener('change', function (e) {
+
         if (e.target.checked) {
-            killLinks();
+            chrome.runtime.sendMessage({ payload: true });
+
         } else {
-            fixLinks();
+            chrome.runtime.sendMessage({ payload: false });
         }
+
+        window.close()
     });
 
     var myLink = document.getElementById('link');
