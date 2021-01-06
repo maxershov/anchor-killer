8dadd4c525fe9be29b1cea34ea0b40dce6ccd026
Max Ershov <maxx95.95@gmail.com>
2021-01-06 16:21:03 +0300

    feat: add check for tabId

--- background.js
+++ background.js
@@ -1,18 +1,20 @@
 var state = false;
+var tabNum = "";
 
 
 // check saved state and apply on page update
 chrome.tabs.onUpdated.addListener(function (tabId) {
-
-    if (state) {
-        disableHref();
+    if (tabNum === tabId) {
+        if (state) {
+            disableHref();
+        }
     }
 });
 
 
 function enableHref() {
 
-    chrome.tabs.executeScript(null, { file: "fix.js" });
+    chrome.tabs.executeScript(tabNum, { file: "fix.js" });
     chrome.browserAction.setIcon({
         path: {
             "16": "img/icon16.png",
@@ -26,7 +28,7 @@ function enableHref() {
 
 function disableHref() {
 
-    chrome.tabs.executeScript(null, { file: "script.js" });
+    chrome.tabs.executeScript(tabNum, { file: "script.js" });
 
     chrome.browserAction.setIcon({
         path: {
@@ -39,21 +41,26 @@ function disableHref() {
 }
 
 
-chrome.browserAction.onClicked.addListener(function () {
-    disableHref();
-});
-
 
 // disable ext after tab close
 chrome.tabs.onRemoved.addListener(function (tabid, removed) {
-    enableHref();
+
+    chrome.browserAction.setIcon({
+        path: {
+            "16": "img/icon16.png",
+            "32": "img/icon32.png",
+            "48": "img/icon48.png",
+            "128": "img/icon128.png"
+        }
+    });
 })
+
 
 // save state from popup and call func
 chrome.runtime.onMessage.addListener(function (request) {
 
     state = request.payload;
-
+    tabNum = request.tabNum;
     if (state) {
         disableHref();
     } else {
--- popup.js
+++ popup.js
@@ -1,21 +1,29 @@
 var checkboxState = false;
+var tabNum = "";
+
+
+// pass active TabId to the background.js  
+chrome.tabs.query({  active: true, currentWindow:true}, function (tabs){
+    var currentTab = tabs[0];
+    tabNum = currentTab.id;
+
+    // enable on click on icon
+    chrome.runtime.sendMessage({ payload: true, tabNum: tabNum });
+});
 
 
 document.addEventListener('DOMContentLoaded', function () {
     var checkbox = document.getElementById('checkbox');
 
-    // enable on click on icon
-    chrome.runtime.sendMessage({ payload: true });
     checkbox.checked = true;
 
 
     checkbox.addEventListener('change', function (e) {
-
         if (e.target.checked) {
-            chrome.runtime.sendMessage({ payload: true });
+            chrome.runtime.sendMessage({ payload: true, tabNum:tabNum });
 
         } else {
-            chrome.runtime.sendMessage({ payload: false });
+            chrome.runtime.sendMessage({ payload: false, tabNum: tabNum });
         }
 
         window.close()
